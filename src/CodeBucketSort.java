a74125c41177d8c6836eab9c71c367144d929297
Alina <yangxiuling_sx@qiyi.com>
2019-01-23 09:24:17 +0800

    Bucket Sort and Counter

---
+++ src/CodeBucketSort.java
@@ -0,0 +1,64 @@
+package src;
+
+import java.util.Arrays;
+
+public class CodeBucketSort {
+    public static void bucketSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int[] bucket = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            bucket[arr[i]]++;
+        }
+        int i = 0;
+        for (int j = 0; j < bucket.length; j++) {
+            while (bucket[j]-- > 0) {
+                arr[i++] = j;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 50;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+            bucketSort(arr1);
+            comparator(arr2);
+            if (!Arrays.equals(arr1, arr2)) {
+                succeed = false;
+                System.out.println(Arrays.toString(arr1));
+                System.out.println(Arrays.toString(arr2));
+                break;
+            }
+        }
+
+        System.out.println(succeed ? "Bingo!!!" : "Sorry");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        System.out.println(Arrays.toString(arr));
+        bucketSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+}
+
